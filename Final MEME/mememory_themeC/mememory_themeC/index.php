6b031f2cbf6829992a26601abbd6b6cf0dac6f47
Lloydied2day <202411848@gordoncollege.edu.ph>
2025-12-12 23:26:35 +0800

    a

--- Final MEME/mememory_themeC/mememory_themeC/index.php
+++ Final MEME/mememory_themeC/mememory_themeC/index.php
@@ -6,7 +6,7 @@ session_start();
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<title>MEMEmory - Start</title>
+<title>MEMEmory - Start (Forest Cottage Theme)</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body class="page-preload">
